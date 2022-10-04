2b79517c1279c2f9fd594093b394a4f7a1280495
Abraham <yabraham180@gmail.com>
2022-10-03 20:50:25 -0600

    se agrego loading

---
+++ components/loading/loading.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next'
+import { useEffect, useState } from 'react';
+import styles from './loading.module.css';
+
+const Loading: NextPage = () => 
+{
+    const [ hidden, setHidden ] = useState(true);
+    
+    useEffect(()=>{
+
+        setTimeout(()=> setHidden(false),2000)
+
+    },[])
+
+    return (
+        <>
+            <div className={`${styles.loading} ${hidden ? '' : styles.hidden}`}>
+                <div className={styles.loading_img}>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Loading;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from 'next'
 import Laoyut from '../components/layout/layout/layout'
 import Inicio from '../components/inicio/inicio'
-
+import Loading from '../components/loading/loading'
 const Home: NextPage = () => {
   return (
-    
+     <>
+      <Loading />   
      <Laoyut>
         <Inicio />
      </Laoyut>
+     </>
     
   )
 }
