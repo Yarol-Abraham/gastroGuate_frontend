694cb2845f71a0b70bf36edf9727cf3e823c393a
Abraham <yabraham180@gmail.com>
2022-10-10 20:52:05 -0600

    ahora se puede menu para usuario

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import { useState } from 'react';
 import styles from './header.module.css';
 import Shopping from './shoping/shopping';
+import User from './user/user';
 
 /*
 _ = indica que es una variable (solo aplica si ya existe otra variable o funcion con el nombre deseado)
@@ -9,10 +10,12 @@ _ = indica que es una variable (solo aplica si ya existe otra variable o funcion
 
 const Header: NextPage = () => {
 
-  const [ _menu, setmenu ] = useState(false)
+  const [ _menu, setmenu ] = useState(false);
   const [ shoping, setShoping ] = useState(false);
+  const [ user, setUser ] = useState(false);
 
   const handleShoping = ()=> setShoping(!shoping);  
+  const handleUser = ()=> setUser(!user);  
 
   return (
     <>
@@ -20,18 +23,19 @@ const Header: NextPage = () => {
       <a href="#" className={styles.logo}><i className="fas fa-utensils"></i>GastroGuate.</a>
 
       <nav className={`${styles.navbar} ${_menu ? styles.active : ''} `}>
-          <a className={styles.active} href="#home">Inicio</a>
-          <a href="#">Menu</a>
-          <a href="#dishes">Regiones</a>
-          <a href="#about">Ofertas</a>
-          <a href="#menu">Vendedores</a>
+          <a className={`${styles.a} ${styles.active}`} href="#home">Inicio</a>
+          <a href="#" className={styles.a}>Menu</a>
+          <a href="#dishes" className={styles.a}>Regiones</a>
+          <a href="#about" className={styles.a}>Ofertas</a>
+          <a href="#menu" className={styles.a}>Vendedores</a>
       </nav>
 
       <div className={styles.icons}>
           <i className={`${styles.i} fas fa-bars ${styles.menu_bars}`} onClick={() => setmenu(!_menu)}></i>
-          <a href="#" className="fas fa-shopping-cart" onClick={handleShoping}></a>
-          <a href="#" className="fas fa-user"></a>
+          <a className={`${styles.a} fas fa-shopping-cart`} onClick={handleShoping}></a>
+          <a className={`${styles.a} fas fa-user`} onClick={handleUser}></a>
           <Shopping shoping={shoping} handleShoping={handleShoping} />
+          { user ? <User /> : null }
       </div>
 
     </header> 
---
+++ components/layout/header/user/user.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import styles from './user.module.css';
+import Link from "next/link";
+
+const User: NextPage  = function () 
+{
+    return (
+        <div className={styles.container_user}>
+            <Link href="/">
+                <a className={styles.a}>Mi perfil</a>
+            </Link>
+            <Link href="/">
+                <a className={styles.a}>Cerrar Sesión</a>
+            </Link>
+            <Link href="/">
+                <a className={styles.a}>Login</a>
+            </Link>   
+        </div>
+    )    
+}
+
+export default User;
